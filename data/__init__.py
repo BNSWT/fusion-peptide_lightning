8609f3753fd1f6040aa70c251bf34c79560fa44d
Yuyang Zhou <zhouyuyang712@outlook.com>
2023-02-20 15:30:04 +0800

    Update git ignore

--- data/__init__.py
+++ data/__init__.py
@@ -1,7 +1,7 @@
 '''
 Author: Yuyang Zhou
 Date: 2023-02-19 11:14:50
-LastEditTime: 2023-02-19 22:06:06
+LastEditTime: 2023-02-19 22:19:45
 Description: 
 Copyright (c) 2023 by Repr. Lab, Westlake University, All Rights Reserved. 
 '''
@@ -9,4 +9,4 @@ Copyright (c) 2023 by Repr. Lab, Westlake University, All Rights Reserved.
 from data.data_interface import DInterface
 from data.dataset_spliter import DatasetSpliter
 from data.sequence_dataset import SequenceDataset
-from visualization import *
+from data.visualization import *
