39594cfd33645fd9bb6941f40d9c8d99431bb0d3
Yuyang Zhou <zhouyuyang712@outlook.com>
2023-02-21 13:22:18 +0000

    Add roc graph in visualization tool

--- data/visualization.py
+++ data/visualization.py
@@ -16,4 +16,14 @@ def distribution(data):
     plt.legend()
     plt.grid(linestyle='--')
     plt.show()
-    
+
+def roc_graph(fpr, tpr, auc):
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % auc)
+    plt.legend(loc = 'lower right')
+    plt.plot([0,1], [0,1], 'r--')
+    plt.xlim([0,1])
+    plt.ylim([0,1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
